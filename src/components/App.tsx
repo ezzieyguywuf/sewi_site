12c2a52ed5d47c90a04d67cc1495d8d2cdc73127
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-10-18 00:05:05 -0400

    navbar: move logo to navbar. also...
    ...change to use div instead of ul
    
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from '../res/sewi_logo.png';
 import './App.css';
 import Navbar from './Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <img src={logo} alt="logo" />
-      </header>
       <Navbar />
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,24 @@
 import './Navbar.css'
 import React from 'react';
+import logo from '../res/sewi_logo.png';
 
 function Navbar() {
   return (
-    <ul>
-      <li><a className="active" href="#!">Home</a></li>
-      <li><a href="#!">Premium</a></li>
-      <li><a href="#!">Catalog</a></li>
-      <li className="right"><a href="#!">Contact</a></li>
-    </ul>
+    <div className="navbar">
+        <img className="navitem" src={logo} alt="logo" />
+        <div className="navitem active">
+          <a href="#!">Home</a>
+        </div>
+        <div className="navitem">
+          <a href="#!">Premium</a>
+        </div>
+        <div className="navitem">
+          <a href="#!">Catalog</a>
+        </div>
+        <div className="navitem rightalign">
+          <a href="#!">Contact</a>
+        </div>
+    </div>
   );
 }
 
