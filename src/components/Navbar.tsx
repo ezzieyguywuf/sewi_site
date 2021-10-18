e056f83a7477b76c124edaad5b943a840d0184e3
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-10-17 22:49:04 -0400

    navbar: add 'active' and 'right' stylings
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,10 @@ import React from 'react';
 function Navbar() {
   return (
     <ul>
-      <li><a href="#!">Home</a></li>
+      <li><a className="active" href="#!">Home</a></li>
       <li><a href="#!">Premium</a></li>
       <li><a href="#!">Catalog</a></li>
+      <li className="right"><a href="#!">Contact</a></li>
     </ul>
   );
 }
