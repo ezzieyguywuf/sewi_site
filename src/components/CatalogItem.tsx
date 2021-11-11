555de3cc20d6a02639f3f5e425c0a2fa0dc897ce
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-10 21:40:13 -0500

    catalog: finish setting up css for catalog-item
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './CatalogItem.css'
-
-export interface TechnicalInformation {
-  name: string,
-  value: string,
-}
+import {CatalogTechnicalInfo, TechnicalInformation} from './CatalogTechnicalInfo'
 
 export interface CatalogProps {
     product_code: string,
@@ -20,6 +16,14 @@ export function CatalogItem(props: CatalogProps) {
   return (
     <div className="catalog-item">
       <img src={props.img_path} alt={props.img_alt}></img>
+      <div className="catalog-price catalog-subitem">${props.price.toFixed(2)}</div>
+      <div className="catalog-brief catalog-subitem">{props.brief}</div>
+      {(() => {if (props.detailed !== undefined) {
+        return <div className="catalog-detailed catalog-subitem">{props.detailed}</div>
+      }})()}
+      <div className="catalog-table catalog-subitem">
+        <CatalogTechnicalInfo data={props.tech}/>
+      </div>
     </div>
   );
 }
