0f0f0e187b47d192ed386316f8a74624d2e9733a
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-11-11 22:02:21 -0500

    catalog: add missing file
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

---
+++ src/components/CatalogTechnicalInfo.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export interface TechnicalInformation {
+  name: string,
+  value: string,
+}
+
+interface TechnicalData {
+  data: TechnicalInformation[]
+}
+
+export function CatalogTechnicalInfo(props: TechnicalData) {
+  const tableRows = props.data.map(({name, value}) => (
+    <tr>
+      <td>{name}</td>
+      <td>{value}</td>
+    </tr>
+  ))
+  if (props.data.length === 0) {
+    return null;
+  } else {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th colSpan={2}>Informacion Téchnica</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableRows}
+        </tbody>
+      </table>
+    );
+  }
+}
