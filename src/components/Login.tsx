164aa0029339281f15a55bceceec1c7256c149ad
Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>
2021-12-21 14:57:31 -0500

    login: check user group
    Signed-off-by: Wolfgang E. Sanyer <WolfgangESanyer@Gmail.com>

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -8,7 +8,10 @@ function Login() {
     <Authenticator>
       {({ signOut, user }) => (
         <main>
-          <h1>Hello {user.username}</h1>
+          {/*
+            // @ts-ignore */}
+          <h1>Hello {user.attributes.email}. You are in group {user.signInUserSession.accessToken.payload["cognito:groups"]}</h1>
+          {console.log(`${JSON.stringify(user, null, 2)}`)}
           <button onClick={signOut}>Sign out</button>
         </main>
       )}
